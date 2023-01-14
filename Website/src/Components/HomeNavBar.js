abf72dfc1c5ccec15d1a67455cdff4813d6c7795
Josiahidk <106179803+Josiahidk@users.noreply.github.com>
2023-01-14 11:09:23 -0400

    update to navbar

--- Website/src/Components/HomeNavBar.js
+++ Website/src/Components/HomeNavBar.js
@@ -1,24 +1,34 @@
+import { Link } from 'react-router-dom'
 import '../style/homeNavbar.css';
 
-const NavBar = () => {
 
-
-    return ( 
+const NavBar = () => { ( 
+<Router>
+        
         <header className = "header">
             <h1 className="header-name">NE1-FREELANCE</h1>
             <nav className= "navBar">
                 <ul>
-                    <li>Home</li>
-                    <li>Browse Jobs</li>
-                    <li>Services</li>
-                    <li>Blogs</li>
-                    <li>About</li>
-                    <li>Pages</li>
-                    <li>Contact</li>
+                    <li>
+                        <Link to ="/home">Home</Link></li>
+                    <li>
+                        <Link to ="/jobs">Browse Jobs</Link></li>
+                    <li>
+                        <Link to="/services">Services</Link></li>
+                    <li>
+                        <Link to ="/blogs">Blogs</Link></li>
+                    <li>
+                        <Link to ="/about">About</Link></li>
+                    <li>
+                        <Link to ="/pages">Pages</Link></li>
+                    <li>
+                        <Link to ="/contact">Contact</Link></li>
                     <li className="dashboard">Dash Board</li>
                 </ul>
             </nav>
+           
         </header>
+        </Router>
     );
 }
 
