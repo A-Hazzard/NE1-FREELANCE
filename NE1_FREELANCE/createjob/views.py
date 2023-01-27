8b32f4992a07da105314826ae4da429826aef3b7
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-01-26 22:11:10 -0800

    Create job form security and home page categories

--- NE1_FREELANCE/createjob/views.py
+++ NE1_FREELANCE/createjob/views.py
@@ -14,15 +14,29 @@ def success(request):
     price = request.GET.get('price')
     category = request.GET.get('category')
 
-    
-    #Creating a job from the user input
-    job_category = JobCategory.objects.get(name=category)
-    job = Job(title=title, description=description, price=price, category=job_category)
+    info = [ title, description, price, category ]
 
-    if job:
-        job.save()
-        print('Job Created')
-    else:
-        print("Error creating job")
-        
-    return HttpResponse("<h1>SUCCESS")
+
+    for i in info:
+        if i == '':
+            print ('\nERROR! Cant leave field empty\n')
+            cannot_leave_space_blank_ERROR = "Do not leave any fields empty" 
+            job_category = JobCategory.objects.all()
+            context = {
+                "error" : cannot_leave_space_blank_ERROR,
+                "job_category" : job_category
+            }
+            return render(request, "createjob/createjob.html", context)
+
+        else:
+            #Creating a job from the user input
+            job_category = JobCategory.objects.get(name=category)
+            job = Job(title=title, description=description, price=price, category=job_category)
+
+            if job:
+                job.save()
+                print('Job Created')
+            else:
+                print("Error creating job")
+                
+            return HttpResponse("<h1>SUCCESS</h1>")
