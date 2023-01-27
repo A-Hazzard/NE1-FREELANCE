53be959d11a10722fffc2c9dfa1c215cda0dca05
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-01-26 16:00:53 -0800

    User can create jobs

--- NE1_FREELANCE/createjob/views.py
+++ NE1_FREELANCE/createjob/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from searchResults.models import Job, JobCategory
 # Create your views here.
 def jobForm(request):
-    return render(request, 'createjob/createjob.html', {})
+    job_category = JobCategory.objects.all()
+    
+    return render(request, 'createjob/createjob.html', {'job_category': job_category})
 
 def success(request):
+    #Fetching the user data that was sent via the HTTP Request
+    title = request.GET.get('title')
+    description = request.GET.get('description')
+    price = request.GET.get('price')
+    category = request.GET.get('category')
+
+    
+    #Creating a job from the user input
+    job_category = JobCategory.objects.get(name=category)
+    job = Job(title=title, description=description, price=price, category=job_category)
+
+    if job:
+        job.save()
+        print('Job Created')
+    else:
+        print("Error creating job")
+        
     return HttpResponse("<h1>SUCCESS")
