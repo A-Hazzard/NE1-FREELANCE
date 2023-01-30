7f184bbc180a3012fe0846f12309388532844cf0
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-01-30 12:52:26 -0800

    removed models

--- NE1_FREELANCE/events/admin.py
+++ NE1_FREELANCE/events/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Venue)
-admin.site.register(MyClubUser())
-admin.site.register(Event)
+# from django.contrib import admin
+# from .models import *
+# # Register your models here.
+# admin.site.register(Venue)
+# admin.site.register(MyClubUser())
+# admin.site.register(Event)
