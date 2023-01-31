41d2f0fbf1b2df7876e7ac6f0a8eb2d14f95da18
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-01-30 22:08:16 -0800

    Added navbar to each page

--- NE1_FREELANCE/home/views.py
+++ NE1_FREELANCE/home/views.py
@@ -15,5 +15,8 @@ def index(request):
     services = Service.objects.all()
     job_category = JobCategory.objects.all()
 
-    # search_jobs(request)
-    return render(request,'home/index.html' , {'services' : services, 'job_category': job_category})
+    if request.user.is_authenticated: #if they are logged in
+        return redirect("search_jobs")
+    else:
+        # search_jobs(request)
+        return render(request,'home/index.html' , {'services' : services, 'job_category': job_category})
