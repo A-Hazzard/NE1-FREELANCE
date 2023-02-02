00fe7f11270be33172c81673327b76790c03a999
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-02-01 18:27:52 -0800

    navbar for each page

--- NE1_FREELANCE/createjob/forms.py
+++ NE1_FREELANCE/createjob/forms.py
@@ -1,11 +1,36 @@
 from django import forms
 from searchResults.models import Job, JobCategory
+from django.utils.safestring import mark_safe
+
+ 
 class CreateJobForm(forms.ModelForm):
-    title = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
-    description = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}), required=True)
-    price = forms.DecimalField(decimal_places=2, widget=forms.NumberInput(attrs={'class': 'form-control'}), required=True)
-    category = forms.ModelChoiceField(queryset=JobCategory.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}), required=True)
-    
+    title = forms.CharField(
+        label='Title',
+        max_length=50, 
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True,
+    )
+    description = forms.CharField(
+        label='Description',
+        max_length=255, 
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True,
+    )
+    price = forms.DecimalField(
+        label='Price',
+        decimal_places=2, 
+        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        required=True,
+    )
+    category = forms.ModelChoiceField(
+        label='Category',
+        queryset=JobCategory.objects.all(), 
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=True,
+    )
+  
+
+
     class Meta:
         model = Job
         fields = ['title', 'description', 'price', 'category']
