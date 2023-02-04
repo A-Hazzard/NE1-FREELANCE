addbeaa81832b6e8dda5a012557f071956d1425d
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-02-03 17:34:51 -0800

    removed backup

--- BACKUP/aboutus/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from ckeditor.fields import RichTextField
-
-# Create your models here.
-
-class AboutUsContent(models.Model):
-    information = RichTextField(config_name='default') ##text formatter for textarea fields. pip install django-ckeditor
-
-    
