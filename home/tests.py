f9ae52dc88c38b76e2a8e8850a77c388d9c91538
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-02-09 01:43:23 -0800

    Added Tests for models

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Service
 
-# Create your tests here.
+
+class ServiceModelTestCase(TestCase):
+    def setUp(self):
+        self.service = Service.objects.create(
+            title='Software Engineer',
+            tagline='Work on an exciting new project',
+            image='some/image/file.png',
+        )
+
+    def test_service_creation(self):
+        self.assertTrue(isinstance(self.service, Service))
+        self.assertEqual(self.service.title, 'Software Engineer')
+        self.assertEqual(self.service.tagline, 'Work on an exciting new project')
+        self.assertEqual(self.service.image, 'some/image/file.png')
--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Profile
+
+class ProfileModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user1', password='pass!123')
+        self.profile = Profile.objects.create(user=self.user)
+
+    def tearDown(self):
+        del self.profile
+        del self.user
+
+    def test_profile_creation(self):
+        """Test the profile model"""
+        self.assertTrue(isinstance(self.profile, Profile))
+        self.assertEqual(self.profile.user.username, 'user1')
+        self.assertEqual(self.profile.profile_picture.name, 'profile_photos/default.png')
+
+
