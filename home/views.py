14767233beb58c8da378cc5afc3eba05b83a69df
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-02-08 19:55:55 -0800

    code changes

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 
-
 from .models import Service
 from searchResults.models import JobCategory
 
--- members/models.py
+++ members/models.py
@@ -8,5 +8,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
     profile_picture = models.ImageField(default = "profile_photos/default.png", upload_to='profile_photos', blank=True, null=True)
 
+
     def __str__(self):
-            return f'{self.user.username} Profile'
+        return f'{self.user.username} Profile'
