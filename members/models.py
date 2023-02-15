c15be5874213e7690a4b6aca73bf9dfc14363233
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-02-15 13:17:49 -0800

    Fixed Profile Bugs

--- members/models.py
+++ members/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 
 # Create your models here.
 
 # models.py
 class Profile(models.Model):
     user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
+    bio = models.CharField(blank=True, max_length=120)
     profile_picture = models.ImageField(default = "profile_photos/default.png", upload_to='profile_photos', blank=True, null=True)
 
 
