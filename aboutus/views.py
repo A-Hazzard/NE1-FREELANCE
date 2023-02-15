ae068a28b09f8cc9859e95b7d015142e1e3404c2
Raushawn <105221990+Raushawn@users.noreply.github.com>
2023-02-14 23:33:14 -0400

    added footer and worked on about us images

--- aboutus/views.py
+++ aboutus/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .models import AboutUsContent
+from .forms import ImageForm
 
 def aboutus(request):
     information = AboutUsContent.objects.get(id=1)
     return render(request,'aboutus/aboutus.html', {'information':information})
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ImageForm()
+    return render(request, 'upload_image.html', {'form': form})
